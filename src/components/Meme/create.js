27a1d97e6ba27afc9208857a8846e708b78d2bb7
ruzheng <ruzheng1990@gmail.com>
2022-05-04 18:43:56 -0400

    Updated posting.js
    Fixed the broken thumbnail for upload.
    Create state for name.
    Create handle for name.
    Add an input section for name of the file.
    Append the name state into data.
    
    co-author-by: Cody, Maura, CJ

--- src/components/Meme/create.js
+++ src/components/Meme/create.js
@@ -1,97 +1,99 @@
-import React, { Component } from 'react'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import { createMeme } from '../../api/meme'
+// DONT USE!!!
 
-class CreateMeme extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      title: '',
-      selected: {},
-      file: {}
+// import React, { Component } from 'react'
+// import Form from 'react-bootstrap/Form'
+// import Button from 'react-bootstrap/Button'
+// import { createMeme } from '../../api/meme'
 
-    }
-  }
+// class CreateMeme extends Component {
+//   constructor (props) {
+//     super(props)
+//     this.state = {
+//       title: '',
+//       selected: {},
+//       file: {}
 
-    handleTitle = (event) => {
-      this.setState({ [event.target.name]: event.target.value })
-    }
+//     }
+//   }
 
-    onFileChange = (event) => {
-      this.setState({
-        selected: event.target.files[0]
-      })
-    }
+//     handleTitle = (event) => {
+//       this.setState({ [event.target.name]: event.target.value })
+//     }
 
-    handleSubmit = (event) => {
-      event.preventDefault()
-      const { user, msgAlert } = this.props
-      const formData = new FormData()
-      formData.append('upload', this.state.selected)
-      console.log(this.state.selected)
-      console.log(this.state.title)
-      console.log(user)
-      console.log(this.state)
-      console.log(formData)
-      createMeme(this.state, user)
-      // .then(() => this.setState({ created: true }))
-        .then(console.log(this.state))
-        .then(() => {
-          msgAlert({
-            heading: 'Meme created',
-            message: 'Nice meme!',
-            variant: 'success'
-          })
-        })
-        .catch((error) => {
-          msgAlert({
-            heading: 'Fail to Create Meme',
-            message: 'Error: ' + error.message,
-            variant: 'danger'
-          })
-        })
-    }
+//     onFileChange = (event) => {
+//       this.setState({
+//         selected: event.target.files[0]
+//       })
+//     }
 
-    render () {
-      const { title } = this.state
+//     handleSubmit = (event) => {
+//       event.preventDefault()
+//       const { user, msgAlert } = this.props
+//       const formData = new FormData()
+//       formData.append('upload', this.state.selected)
+//       console.log(this.state.selected)
+//       console.log(this.state.title)
+//       console.log(user)
+//       console.log(this.state)
+//       console.log(formData)
+//       createMeme(this.state, user)
+//       // .then(() => this.setState({ created: true }))
+//         .then(console.log(this.state))
+//         .then(() => {
+//           msgAlert({
+//             heading: 'Meme created',
+//             message: 'Nice meme!',
+//             variant: 'success'
+//           })
+//         })
+//         .catch((error) => {
+//           msgAlert({
+//             heading: 'Fail to Create Meme',
+//             message: 'Error: ' + error.message,
+//             variant: 'danger'
+//           })
+//         })
+//     }
 
-      //   if (created) {
-      //     return <Redirect to={'/movies'} />
-      //   }
-      return (
-        <div className='App'>
-          {/* {file.url
-            ? (
-              <img className='displayImg' alt='uploaded file' src={file.url} />
-            )
-            : (
-              ''
-            )} */}
-          {/* {loading ? (<img alt="loading gif" src="https://miro.medium.com/max/882/1*9EBHIOzhE1XfMYoKz1JcsQ.gif"/>) : ''} */}
-          {/* <div className='col-sm-10 col-md-8 mx-auto mt-5'> */}
-          <h3>Create Meme</h3>
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Group controlId='title'>
-              <Form.Label>Title</Form.Label>
-              <Form.Control
-                required
-                name='title'
-                value={title}
-                placeholder='Title'
-                onChange={this.handleTitle}
-              />
-            </Form.Group>
-            <Form.Group>
-              {/* <Form.File type="file" id="fileUpload" label="Upload File Here" onChange={this.handleChange}/> */}
-              <input type='file' onChange={this.onFileChange} />
-            </Form.Group>
-            <Button variant='success' type='submit'>Submit</Button>
-          </Form>
-          {/* </div> */}
-        </div>
-      )
-    }
-}
+//     render () {
+//       const { title } = this.state
 
-export default CreateMeme
+//       //   if (created) {
+//       //     return <Redirect to={'/movies'} />
+//       //   }
+//       return (
+//         <div className='App'>
+//           {/* {file.url
+//             ? (
+//               <img className='displayImg' alt='uploaded file' src={file.url} />
+//             )
+//             : (
+//               ''
+//             )} */}
+//           {/* {loading ? (<img alt="loading gif" src="https://miro.medium.com/max/882/1*9EBHIOzhE1XfMYoKz1JcsQ.gif"/>) : ''} */}
+//           {/* <div className='col-sm-10 col-md-8 mx-auto mt-5'> */}
+//           <h3>Create Meme</h3>
+//           <Form onSubmit={this.handleSubmit}>
+//             <Form.Group controlId='title'>
+//               <Form.Label>Title</Form.Label>
+//               <Form.Control
+//                 required
+//                 name='title'
+//                 value={title}
+//                 placeholder='Title'
+//                 onChange={this.handleTitle}
+//               />
+//             </Form.Group>
+//             <Form.Group>
+//               {/* <Form.File type="file" id="fileUpload" label="Upload File Here" onChange={this.handleChange}/> */}
+//               <input type='file' onChange={this.onFileChange} />
+//             </Form.Group>
+//             <Button variant='success' type='submit'>Submit</Button>
+//           </Form>
+//           {/* </div> */}
+//         </div>
+//       )
+//     }
+// }
+
+// export default CreateMeme
--- src/components/Meme/posting.js
+++ src/components/Meme/posting.js
@@ -9,27 +9,32 @@ import Button from 'react-bootstrap/Button'
 // data.append('upload', selected)
 // in the lesson Eron does this under App js which means this will render when the page opens, are we having an issue because it isn't rendering the default
 
-function PostMeme () {
+function PostMeme ({ user }) {
   const [selected, setSelected] = useState({})
-  const [file, setFile] = useState({})
+  const [file, setFile] = useState(null)
   const [loading, setLoading] = useState(false)
-
+  const [name, setName] = useState('')
   const handleSubmit = (event) => {
     event.preventDefault()
     setLoading(true)
     const data = new FormData()
     console.log(selected)
     data.append('upload', selected)
+    data.append('name', name)
     console.log(data)
     axios({
       url: `${apiUrl}/uploads`,
       method: 'POST',
       data,
       headers: {
+        Authorization: `Bearer ${user.token}`,
         'Content-Type': 'multipart/form-data'
       }
     })
-      .then((res) => setFile(res.data.upload.url))
+      .then((res) => {
+        console.log(res.data.upload)
+        setFile(res.data.upload.url)
+      })
     //   .then((res) => console.log(res.data.upload.url))
       .then(() => setLoading(false))
       .catch(console.error)
@@ -38,16 +43,19 @@ function PostMeme () {
   const handleChange = (event) => {
     setSelected(event.target.files[0])
   }
-  // changed this back to event.target.name need to see if this is correct
-  // also I think we have this line 40 set up like we are using this.setState but I think this hook would have a different syntax than this.setState. I think line 41 would be setTitle(event.target.name=event.target.value)
-  // I also think we need to turn this into a class component and pass in user because that is how we create owner on the server side
+
+  const handleName = (event) => {
+    setName(event.target.value)
+  }
 
   return (
     <>
       <div className='App'>
         {file
           ? (
-            <img className='displayImg' alt='uploaded file' src={file} />
+            <>
+              <img className='displayImg' alt='uploaded file' src={file} />
+            </>
           )
           : (
             ''
@@ -63,8 +71,20 @@ function PostMeme () {
             ''
           )}
         <Form onSubmit={handleSubmit}>
+          <Form.Group controlId='name'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              required
+              name='name'
+              value={name}
+              placeholder='The Meme Name'
+              onChange={handleName}
+            />
+          </Form.Group>
           <Form.Group>
-            <input id="fileUpload" label="Upload File Here" type='file' onChange={handleChange} />
+            <Form.Label>Upload your image</Form.Label>
+            <Form.Control id="fileUpload" type="file" label="Upload File Here" onChange={handleChange}/>
+            {/* <input id="fileUpload" label="Upload File Here" type='file' onChange={handleChange} /> */}
           </Form.Group>
           <Button variant='primary' type='submit'>Submit</Button>
         </Form>
